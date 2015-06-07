f7114e7d07400f06c212de82a7b6191d46c9a0be
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-06-07 08:32:02 -0700

    Attempts to make contact form work. Changes to index, which were reverted back and changes to handleFormSubmit.php.

--- assets/php/handleFormSubmit.php
+++ assets/php/handleFormSubmit.php
@@ -1,10 +1,25 @@
 <?php
 
-if(isset($_POST['c_name'])){
-    
-    $res['sendstatus'] = 1;
-    $res['message'] = 'Form Submission Succesful';
-    echo json_encode($res);
+// if(isset($_POST['c_name'])){
+
+//     $res['sendstatus'] = 1;
+//     $res['message'] = 'Form Submission Succesful';
+//     echo json_encode($res);
+// }
+
+if($_POST["submit"]) {
+    $recipient="jpgalves1@gmail.com";
+    $subject="Form to email message";
+    $c_name=$_POST["c_name"];
+    $c_email=$_POST["c_email"];
+    $c_message=$_POST["c_message"];
+
+    $mailBody="Name: $c_name\nEmail: $c_email\n\n$c_message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$c_email>");
+
+    // $thankYou="<p>Thank you! Your message has been sent.</p>";
 }
+
 
 ?>
